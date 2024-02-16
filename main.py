80b3d9d370abef6d13286028dcb0e5fb50ba5e1f
Carlo <88803350+Carlo-Codes@users.noreply.github.com>
2024-02-16 14:46:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import vlc
 import keyboard
+import os
 keyPressed = False
 loop = True
-# Create the VLC instance and media player
+
 vlc_instance = vlc.Instance('--no-xlib')
 player = vlc_instance.media_player_new()
+username = 'root'
+usbPath = '/media/'+ username +'/VIDEOS/'
 
-# Variable to store the path of the current video
 current_video_path = ""
-initial_video_path = "/data/1.mp4"
+initial_video_path = usbPath + "1.mp4"
 
 def check_stop():
     global loop
     if keyboard.is_pressed('z'):
         player.stop()
-        loop = False
+        vlc_instance.release()
+        loop=False
     
+def checkMediaAttached():
+    global loop
+    for d in os.listdir('/media/'+ username):
+        if d == 'VIDEOS':
+            loop = True
+            return
+    print('VIDEOS usb is not found,')
+    loop = False
+    return
 
 def setVideoKey(player: vlc.MediaPlayer, key: str, videoPath: str):
     try: 
@@ -38,12 +50,10 @@ def check_end(player: vlc.MediaPlayer):
     global current_video_path
     if player.get_state() == vlc.State.Ended:
         if current_video_path:
-            # If the video has ended, loop the current video
             video = vlc_instance.media_new(current_video_path)
             player.set_media(video)
             player.play()
 
-  # Specify the path to the initial video
 current_video_path = initial_video_path
 initial_video = vlc_instance.media_new(initial_video_path)
 player.set_media(initial_video)
@@ -52,13 +62,17 @@ player.play()
 
 
 while loop:
+    checkMediaAttached()
     check_stop()
-    setVideoKey(player, 'w', '/data/2.mp4')
-    setVideoKey(player, 'u', '/data/7.mp4')
-    setVideoKey(player, '+', '/data/5.mp4')
-    setVideoKey(player, 'y', '/data/6.mp4')
-    setVideoKey(player, 'e', '/data/3.mp4')
-    setVideoKey(player, 'r', '/data/4.mp4')
-    setVideoKey(player, 'i', '/data/8.mp4')
+    setVideoKey(player, 'w', usbPath + '2.mp4')
+    setVideoKey(player, 'u', usbPath + '7.mp4')
+    setVideoKey(player, '+', usbPath + '5.mp4')
+    setVideoKey(player, 'y', usbPath + '6.mp4')
+    setVideoKey(player, 'e', usbPath + '3.mp4')
+    setVideoKey(player, 'r', usbPath + '4.mp4')
+    setVideoKey(player, 'i', usbPath + '8.mp4')
+    setVideoKey(player, 'o', usbPath + '1.mp4')
+    setVideoKey(player, 'p', usbPath + '1.mp4')
+
 
     check_end(player)
